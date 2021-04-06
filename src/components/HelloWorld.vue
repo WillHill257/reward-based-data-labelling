bb8c095e1111b83f0f2b6377b2fd13ccb88ef149
andrewboyley <andy.boyley@gmail.com>
2021-04-06 22:42:11 +0200

    Included simple unit test

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,6 +14,7 @@
         <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
 
         <p>{{ somevalue }}</p>
+        <p>{{ msg }}</p>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
@@ -80,7 +81,7 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: "HelloWorld",
-
+  props: ["msg"],
   data: () => ({
     somevalue: "Filler Text",
     ecosystem: [
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,14 @@
 import { expect } from "chai";
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 describe("HelloWorld.vue", () => {
-  it("renders props.msg when passed", () => {
+  it("It renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
+      mocks: { $http: axios },
     });
     expect(wrapper.text()).to.include(msg);
   });
