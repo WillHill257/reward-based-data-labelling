5a6febd13082334ba79ac586a7ac14612fa1ddfa
Iretonsama <iretonsama@gmail.com>
2021-04-11 17:51:12 +0200

    changed how tests are handeled

--- tests/unit/signup.spec.ts
+++ tests/unit/signup.spec.ts
@@ -1,72 +1,58 @@
 import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
+import {
+  createLocalVue,
+  createWrapper,
+  mount,
+  shallowMount,
+} from "@vue/test-utils";
 import Signup from "@/views/Sign_up.vue";
 import axios from "axios";
+import Vue from "vue";
+import Vuetify from "vuetify";
+import nock from "nock";
+import sinon from "sinon";
 
-describe("Testing UI elements of sign up screen", () => {
-  // Tests if there is username input field
-  it("should display an input field for username", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
-    });
-    expect(
-      wrapper.find("#signup-username-input").exists(),
-      'Element with ID "signup-username-input" is not found in the DOM, but is required.'
-    ).to.equal(true);
+declare const global: any;
+Vue.use(Vuetify);
+describe("Testing sign up screen", () => {
+  const localVue = createLocalVue();
+  let vuetify: Vuetify;
+  beforeEach(() => {
+    global.requestAnimationFrame = (cb: any) => cb();
+    vuetify = new Vuetify();
   });
 
-  // Tests if there is email input field
-  it("should display an input field for email", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
+  describe("when loaded", () => {
+    it("should have all the necessary UI elements", () => {
+      const wrapper = mount(Signup, { localVue, vuetify });
+      expect(
+        wrapper.find("#signup-firstname-input").exists(),
+        'Element with ID "signup-firstname-input" is not found in the DOM, but is required.'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-surname-input").exists(),
+        'Element with ID "signup-surname-input" is not found in the DOM, but is required.'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-email-input").exists(),
+        'Element with ID "signup-email-input" is not found in the DOM, but is required'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-password-input").exists(),
+        'Element with ID: "signup-password-input" is not found in the DOM, but is required'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-confirmpassword-input").exists(),
+        'Element with ID: "signup-confirmpassword-input" is not found in the DOM, but is required'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-confirm-button").exists(),
+        'Element with ID: "signup-confirmation-button" is not found in the DOM, but is required'
+      ).to.equal(true);
+      expect(
+        wrapper.find("#signup-cancel-button").exists(),
+        'Element with ID: "signup-cancel-button" is not found in the DOM, but is required'
+      ).to.equal(true);
     });
-    expect(
-      wrapper.find("#signup-email-input").exists(),
-      'Element with ID "signup-email-input" is not found in the DOM, but is required'
-    ).to.equal(true);
-  });
-
-  // Tests if there is password input field
-  it("should display an input field for password", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
-    });
-    expect(
-      wrapper.find("#signup-password-input").exists(),
-      'Element with ID: "signup-password-input" is not found in the DOM, but is required'
-    ).to.equal(true);
-  });
-
-  // Tests if there is password confirm input field
-  it("should display an input field for password confirmation", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
-    });
-    expect(
-      wrapper.find("#signup-confirmpassword-input").exists(),
-      'Element with ID: "signup-confirmpassword-input" is not found in the DOM, but is required'
-    ).to.equal(true);
-  });
-
-  // Tests if there is a button to confirm sign up
-  it("should display a button to confirm sign up", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
-    });
-    expect(
-      wrapper.find("#signup-confirm-button").exists(),
-      'Element with ID: "signup-confirmation-button" is not found in the DOM, but is required'
-    ).to.equal(true);
-  });
-
-  // Tests if there is a button to cancel sign up
-  it("should display a button to cancel sign up", () => {
-    const wrapper = shallowMount(Signup, {
-      mocks: { $http: axios },
-    });
-    expect(
-      wrapper.find("#signup-cancel-button").exists(),
-      'Element with ID: "signup-cancel-button" is not found in the DOM, but is required'
-    ).to.equal(true);
   });
 });
