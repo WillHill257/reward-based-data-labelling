2264189e1114d46a4e1e3cd110431d8c32c70129
Iretonsama <iretonsama@gmail.com>
2021-04-16 16:19:15 +0200

    migrated to jest for frontend

--- tests/unit/uploadimage.spec.ts
+++ tests/unit/uploadimage.spec.ts
@@ -1,5 +1,12 @@
+import { shallowMount } from "@vue/test-utils";
 import UploadImage from "@/components/CreateJob/components/UploadImage.vue";
+import Vue from "vue";
+import Vuetify from "vuetify";
 
-// describe("when upload image component is loaded", () => {
-// 	const wrapper =
-// });
+Vue.use(Vuetify);
+
+describe("when upload image component is loaded", () => {
+  // const wrapper = shallowMount(UploadImage);
+  test.todo("should have an drag and drop area for file uploading");
+  test.todo("should store the files uploaded");
+});
