1515e838b0f55b6fdde77bc2468724405ce2c863
Speed-dy <james.alence@gmail.com>
2021-04-17 19:48:23 +0200

    made CreateJobs screen a popup in homePage

--- src/components/CreateJob/CreateJob.vue
+++ src/components/CreateJob/CreateJob.vue
@@ -1,25 +1,60 @@
-
 <template>
-  <v-content style="
-  padding-left: 4%;
-  padding-top: 2%;
-  padding-right: 4%" >
+  <v-dialog
+    v-model="isShowDialog"
+    @click:outside="closeDialog">
+    <v-card id="createJobCard">
+      <v-row>
+        <v-col>
+          <v-text-field
+            v-model="title"
+            label="Title"
+            id="title-input">
+          </v-text-field>
 
-    
-    <v-text-field
-    label="Title"
-    id = "title-input">
-    </v-text-field>
+          <v-textarea
+            v-model="description"
+            label="Description"
+            id="description-input">
+          </v-textarea>
 
-    <v-textarea
-      label="Description"
-      id = "description-input">
-    </v-textarea>
-
-    <v-card-actions style="padding-left: 80%; padding-top: 25%">
-    <v-btn color="green" id = "submit-input">Submit</v-btn>
-    <v-btn color="grey" id = "discard-input">Discard</v-btn>
-    </v-card-actions>
-  </v-content>
+          <v-card-actions style="padding-left: 80%; padding-top: 25%">
+            <v-btn color="green" id="submit-input">Submit</v-btn>
+            <v-btn color="grey" id="discard-input">Discard</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-dialog>
 </template>
+
+<script>
+export default {
+  name: "CreateJob",
+
+  data() {
+    return { title: "", description: "" };
+  },
+
+  props: {
+    isShowDialog: {
+      type: Boolean,
+      required: true
+    }
+  },
+
+  methods: {
+    closeDialog() {
+      this.$emit("update:isShowDialog", false);
+      this.title = "";
+      this.description = "";
+    }
+  }
+};
+</script>
+<style scoped>
+#createJobCard {
+  padding: 2%
+}
+</style>
+
 
