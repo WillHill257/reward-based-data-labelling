0834bdc63568f0c41003c0040aa0bb12586ed110
Iretonsama <iretonsama@gmail.com>
2021-04-17 21:15:47 +0200

    basic file uploading functionality with job creator;

--- src/components/CreateJob/CreateJob.vue
+++ src/components/CreateJob/CreateJob.vue
@@ -1,24 +1,23 @@
 <template>
-  <v-dialog
-    v-model="isShowDialog"
-    @click:outside="closeDialog">
+  <v-dialog width="80%" v-model="isShowDialog" @click:outside="closeDialog">
     <v-card id="createJobCard">
-      <v-row>
+      <v-row justify="center" align="center" no-gutters>
+        <v-col> <ImageUploader :onFilesUploaded="onFilesUploaded" /></v-col>
         <v-col>
-          <v-text-field
-            v-model="title"
-            label="Title"
-            id="title-input">
+          <v-text-field v-model="title" label="Title" id="title-input">
           </v-text-field>
 
           <v-textarea
             v-model="description"
             label="Description"
-            id="description-input">
+            id="description-input"
+          >
           </v-textarea>
 
-          <v-card-actions style="padding-left: 80%; padding-top: 25%">
-            <v-btn color="green" id="submit-input">Submit</v-btn>
+          <v-card-actions style="padding-top: 25%">
+            <v-btn color="green" id="submit-input" @click="onSubmitClicked"
+              >Submit</v-btn
+            >
             <v-btn color="grey" id="discard-input">Discard</v-btn>
           </v-card-actions>
         </v-col>
@@ -27,34 +26,46 @@
   </v-dialog>
 </template>
 
-<script>
-export default {
-  name: "CreateJob",
+<script lang="ts">
+import Vue from "vue";
+import ImageUploader from "./components/ImageUploader.vue";
 
+export default Vue.extend({
+  name: "CreateJob",
+  components: { ImageUploader },
   data() {
-    return { title: "", description: "" };
+    return {
+      title: "",
+      description: "",
+      filesUploaded: [],
+    };
   },
 
   props: {
     isShowDialog: {
       type: Boolean,
-      required: true
-    }
+      required: true,
+    },
   },
 
   methods: {
-    closeDialog() {
+    closeDialog(): void {
       this.$emit("update:isShowDialog", false);
       this.title = "";
       this.description = "";
-    }
-  }
-};
+      this.filesUploaded = [];
+    },
+    onSubmitClicked(): void {
+      console.log(this.filesUploaded);
+    },
+    onFilesUploaded(files: never): void {
+      this.filesUploaded.push(files);
+    },
+  },
+});
 </script>
 <style scoped>
 #createJobCard {
-  padding: 2%
+  padding: 2%;
 }
 </style>
-
-
--- src/components/CreateJob/components/ImageUploader.vue
+++ src/components/CreateJob/components/ImageUploader.vue
@@ -1,14 +1,42 @@
 <template>
-  <v-dialog v-model="dialog">
-    <v-card>
-      <v-card-text> </v-card-text>
-      <v-card-actions> </v-card-actions>
-    </v-card>
-  </v-dialog>
+  <v-container
+    @drop.prevent="onDrop($event)"
+    @dragover.prevent="dragover = true"
+    @dragenter.prevent="dragover = true"
+    @dragleave.prevent="dragover = false"
+    :class="{ 'grey lighten-2': dragover }"
+  >
+    <v-card-text>
+      <v-col align="center" justify="center">
+        <v-icon class="mt-5" size="60">mdi-cloud-upload</v-icon>
+        <p>Drop your file(s) here, or click to select them.</p>
+      </v-col>
+    </v-card-text>
+  </v-container>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
   name: "ImageUploader",
-};
+  props: {
+    onFilesUploaded: {
+      type: Function,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      dragover: false,
+      uploadedFiles: [],
+    };
+  },
+  methods: {
+    onDrop(e: any) {
+      console.log("hey");
+      this.dragover = false;
+      e.dataTransfer.files.forEach((element) => this.onFilesUploaded(element));
+    },
+  },
+});
 </script>
