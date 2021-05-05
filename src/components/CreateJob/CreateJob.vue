79ee9199102cfcc646509a7ab727b82ed964bf61
Iretonsama <iretonsama@gmail.com>
2021-05-05 22:53:52 +0200

    basic image uploading

--- src/components/CreateJob/CreateJob.vue
+++ src/components/CreateJob/CreateJob.vue
@@ -1,61 +1,61 @@
 <template>
   <v-dialog width="80%" v-model="isShowDialog" @click:outside="closeDialog">
     <v-card id="createJobCard">
-      <v-row justify="center" align="center" no-gutters>
-        <v-col align="start" class="mx-2">
-          <ImageUploader
-            v-bind:filesUploaded="filesUploaded"
-            :onFilesUploaded="onFilesUploaded"
-          />
+      <form @submit.prevent="onSubmitClicked" enctype="multipart/form-data">
+        <v-row justify="center" align="center" no-gutters>
+          <v-col align="start" class="mx-2">
+            <ImageUploader
+              v-bind:filesUploaded="filesUploaded"
+              :onFilesUploaded="onFilesUploaded"
+            />
 
-          <v-list dense id="image-list" class="mt-1">
-            <v-list-item v-for="(file, i) in filesUploaded" :key="i">
-              <v-list-item-icon>
-                <v-icon>mdi-file-image</v-icon>
-              </v-list-item-icon>
-              <v-list-item-content>
-                <v-list-item-title v-text="file.name"></v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-        </v-col>
+            <v-list dense id="image-list" class="mt-1">
+              <v-list-item v-for="(file, i) in filesUploaded" :key="i">
+                <v-list-item-icon>
+                  <v-icon>mdi-file-image</v-icon>
+                </v-list-item-icon>
+                <v-list-item-content>
+                  <v-list-item-title v-text="file.name"></v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list>
+          </v-col>
 
-        <v-col>
-          <!--          <v-alert-->
-          <!--            :style="{ visibility: errorVisibility }"-->
-          <!--            :height="errorHeight"-->
-          <!--            dense-->
-          <!--            dismissible-->
-          <!--            outlined-->
-          <!--            type="warning"-->
-          <!--          >{{ errorMessage}}</v-alert-->
-          <!--          >-->
-          <v-text-field v-model="title" label="Title" id="title-input">
-          </v-text-field>
+          <v-col>
+            <!--          <v-alert-->
+            <!--            :style="{ visibility: errorVisibility }"-->
+            <!--            :height="errorHeight"-->
+            <!--            dense-->
+            <!--            dismissible-->
+            <!--            outlined-->
+            <!--            type="warning"-->
+            <!--          >{{ errorMessage}}</v-alert-->
+            <!--          >-->
+            <v-text-field v-model="title" label="Title" id="title-input">
+            </v-text-field>
 
-          <v-textarea
-            v-model="description"
-            label="Description"
-            id="description-input"
-          >
-          </v-textarea>
-
-          <v-card-actions style="padding-top: 25%">
-            <v-btn
-              color="green"
-              id="submit-input"
-              v-on:click.native="onSubmitClicked"
-              >Submit</v-btn
+            <v-textarea
+              v-model="description"
+              label="Description"
+              id="description-input"
             >
-            <v-btn
-              color="grey"
-              id="discard-input"
-              v-on:click.native="closeDialog"
-              >Discard</v-btn
-            >
-          </v-card-actions>
-        </v-col>
-      </v-row>
+            </v-textarea>
+
+            <v-card-actions style="padding-top: 25%">
+              <v-btn color="green" id="submit-input" type="submit">
+                Submit
+              </v-btn>
+              <v-btn
+                color="grey"
+                id="discard-input"
+                v-on:click.native="closeDialog"
+              >
+                Discard
+              </v-btn>
+            </v-card-actions>
+          </v-col>
+        </v-row>
+      </form>
     </v-card>
   </v-dialog>
 </template>
@@ -93,17 +93,40 @@ export default Vue.extend({
       this.filesUploaded = [];
     },
     onSubmitClicked: function () {
-      if (this.title == "") {
-        this.errorMessage = "Title required";
-      } else if (this.description == "") {
-        this.errorMessage = "Description required";
-      } else if (this.filesUploaded.length == 0) {
-        this.errorMessage = "No files to upload";
-      } else {
-        return;
+      // checks if all fields are filled
+
+      // if (this.title == "") {
+      //   this.errorMessage = "Title required";
+      // } else if (this.description == "") {
+      //   this.errorMessage = "Description required";
+      // } else if (this.filesUploaded.length == 0) {
+      //   this.errorMessage = "No files to upload";
+      // } else {
+      //   return;
+      // }
+
+      let formData = new FormData();
+      for (var i in this.filesUploaded) {
+        let file = this.filesUploaded[i];
+        formData.append("image", file);
       }
+
       this.errorVisibility = "visible";
       this.errorHeight = 40;
+
+      const url = "http://localhost:4000/api/images/upload";
+      this.axios
+        .post(url, formData, {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     },
     onFilesUploaded(file: File): void {
       this.filesUploaded.push(file);
