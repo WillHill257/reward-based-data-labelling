e2a360bd2fb29f22292d2b49bec7b8de9689b4fa
William Hill <whill4@me.com>
2021-05-10 01:19:15 +0200

    Added API call to get all current jobs

--- src/views/ViewJobs.vue
+++ src/views/ViewJobs.vue
@@ -1,54 +1,33 @@
 <template>
-    <v-app>
-  
+  <v-app>
     <h1>Jobs</h1>
 
     <v-container grid-list-lg>
-
-      <v-layout 
-      row wrap>
-        <v-flex 
-        xs12 sm6 md4 lg3 
-        v-for="job in jobs" :key="job.title">
-          <v-card 
-          elevation="5" 
-          width="300px"
-          height ="200px">
-
-            <v-card-title 
-            id = "job-title">
-            {{job.title}}
+      <v-layout row wrap>
+        <v-flex xs12 sm6 md4 lg3 v-for="job in jobs" :key="job._id">
+          <v-card elevation="5" width="300px" height="200px">
+            <v-card-title id="job-title">
+              {{ job.title }}
             </v-card-title>
 
-            <v-card-subtitle 
-            id = "job-type">
-                {{job.type}}
-                </v-card-subtitle>
+            <v-card-subtitle id="job-type">
+              {{ job.type }}
+            </v-card-subtitle>
 
-            <v-card-text 
-            >
-              <v-clamp id = "job-description" autoresize :max-lines="3">{{ job.description }}</v-clamp>
-              </v-card-text>
+            <v-card-text>
+              <v-clamp id="job-description" autoresize :max-lines="3">{{
+                job.description
+              }}</v-clamp>
+            </v-card-text>
 
-            <v-card-actions 
-            class ="card-actions"
-            flat >
-            <v-btn
-            id ="btn-view-job"
-            color = "blue"
-            text>
-                View job
-                </v-btn>
-               </v-card-actions>
-               
+            <v-card-actions class="card-actions" flat>
+              <v-btn id="btn-view-job" color="blue" text> View job </v-btn>
+            </v-card-actions>
           </v-card>
         </v-flex>
       </v-layout>
-
     </v-container>
-    
- 
-    </v-app>
+  </v-app>
 </template>
 
 <style>
@@ -60,39 +39,41 @@
 
 <script>
 import VClamp from "vue-clamp";
+import axios from "axios";
+
 export default {
   components: {
-    VClamp
+    VClamp,
   },
   data() {
     return {
-      jobs: [
-        { title: 'Title', type: 'Type', description: 'Description' },
-        { title: 'Dog vs Cats', type: 'Image', description: 'Identify whether an image is one of a cat or a dog.eufhef usin fesuif seuibfsi uebfsi uebfsi uebfui sbiuebfsi ufuebufbciefbicsbf' },
-        { title: 'Clouds', type: 'Image', description: 'Identify the types of clouds in an image.' },
-        { title: 'Parts of speech', type: 'Text', description: 'Identify Parts of speech when given a word fioen nfeio foie nosfao nfi oesfn slkfei n sif nensn fsen fosieefinceinfone cienfencf'},
-        { title: 'Farm animals', type: 'Image', description: 'Identify the farm animal when given an image ienfos f ein oseif snefn sienfos efnienf s cfiencubfuebicfebcfbec ceubfciue ceocf' },
-        { title: 'Digits', type: 'Image', description: 'Identify hand written number in an image'},
-        { title: 'Title', type: 'Type', description: 'Description' },
-        { title: 'Dog vs Cats', type: 'Image', description: 'Identify whether an image is one of a cat or a dog.eufhef usin fesuif seuibfsi uebfsi uebfsi uebfui sbiuebfsi ufuebufbciefbicsbf' },
-        { title: 'Clouds', type: 'Image', description: 'Identify the types of clouds in an image.' },
-        { title: 'Parts of speech', type: 'Text', description: 'Identify Parts of speech when given a word fioen nfeio foie nosfao nfi oesfn slkfei n sif nensn fsen fosieefinceinfone cienfencf'},
-        { title: 'Farm animals', type: 'Image', description: 'Identify the farm animal when given an image ienfos f ein oseif snefn sienfos efnienf s cfiencubfuebicfebcfbec ceubfciue ceocf' },
-        { title: 'Digits', type: 'Image', description: 'Identify hand written number in an image'},
-        { title: 'Title', type: 'Type', description: 'Description' },
-        { title: 'Dog vs Cats', type: 'Image', description: 'Identify whether an image is one of a cat or a dog.eufhef usin fesuif seuibfsi uebfsi uebfsi uebfui sbiuebfsi ufuebufbciefbicsbf' },
-        { title: 'Clouds', type: 'Image', description: 'Identify the types of clouds in an image.' },
-        { title: 'Parts of speech', type: 'Text', description: 'Identify Parts of speech when given a word fioen nfeio foie nosfao nfi oesfn slkfei n sif nensn fsen fosieefinceinfone cienfencf'},
-        { title: 'Farm animals', type: 'Image', description: 'Identify the farm animal when given an image ienfos f ein oseif snefn sienfos efnienf s cfiencubfuebicfebcfbec ceubfciue ceocf' },
-        { title: 'Digits', type: 'Image', description: 'Identify hand written number in an image'},
-        { title: 'Title', type: 'Type', description: 'Description' },
-        { title: 'Dog vs Cats', type: 'Image', description: 'Identify whether an image is one of a cat or a dog.eufhef usin fesuif seuibfsi uebfsi uebfsi uebfui sbiuebfsi ufuebufbciefbicsbf' },
-        { title: 'Clouds', type: 'Image', description: 'Identify the types of clouds in an image.' },
-        { title: 'Parts of speech', type: 'Text', description: 'Identify Parts of speech when given a word fioen nfeio foie nosfao nfi oesfn slkfei n sif nensn fsen fosieefinceinfone cienfencf'},
-        { title: 'Farm animals', type: 'Image', description: 'Identify the farm animal when given an image ienfos f ein oseif snefn sienfos efnienf s cfiencubfuebicfebcfbec ceubfciue ceocf' },
-        { title: 'Digits', type: 'Image', description: 'Identify hand written number in an image'},
-      ]
-    }
-  }
-}
+      jobs: [{ title: "Title", type: "Type", description: "Description" }],
+    };
+  },
+  methods: {
+    getAllJobs() {
+      const config = {
+        method: "get",
+        url: "http://localhost:4000/api/job",
+        headers: {},
+      };
+
+      axios(config)
+        .then((response) => {
+          this.jobs = response.data;
+          for (let i = 0; i < this.jobs.length; i++) {
+            this.jobs[i].type = "Image";
+          }
+          // console.log(JSON.stringify(response.data));
+          console.log(this.jobs);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  mounted() {
+    this.getAllJobs();
+  },
+};
 </script>
