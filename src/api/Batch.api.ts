3b56bc0327af358648c33695bf3b33c6518a10b0
Iretonsama <iretonsama@gmail.com>
2021-08-28 15:11:18 +0200

    added batch accepting logic

--- src/api/Batch.api.ts
+++ src/api/Batch.api.ts
@@ -5,9 +5,10 @@ const END_POINT = "/batch";
 const acceptBatch = (jobId: string): Promise<any> => {
   console.log("Accepting job...");
 
-  return httpClient.get("/next/" + jobId).then((res: any) => {
+  return httpClient.get(END_POINT + "/next/" + jobId).then((res: any) => {
     if (res.status != 400) {
-      return httpClient.put("/labeller/" + res.body._id);
+      console.log(res);
+      return httpClient.put(END_POINT + "/labeller/" + res.data._id);
     }
   });
 };
